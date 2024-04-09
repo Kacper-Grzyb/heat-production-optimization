b21f9827665660e4c0aef23764ed00cd8b088f75
KacperGrzyb2137 <kgrzyb2860@gmail.com>
2024-04-09 22:04:48 +0200

    Added in-memory database and user file upload

--- heat-production-optimization/Models/SourceDataDbContext.cs
+++ heat-production-optimization/Models/SourceDataDbContext.cs
@@ -5,6 +5,9 @@ namespace heat_production_optimization.Models
 {
     public class SourceDataDbContext : DbContext
     {
+        // TODO add user input to save here
+        public DbSet<HeatDemandDataModel> HeatDemandData { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseInMemoryDatabase("CalculatorAppDb");
--- heat-production-optimization/Pages/Index.cshtml.cs
+++ heat-production-optimization/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using heat_production_optimization.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,15 +6,44 @@ namespace heat_production_optimization.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly SourceDataDbContext _context = new SourceDataDbContext();
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public IFormFile formFile { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, SourceDataDbContext context)
         {
             _logger = logger;
+            if (context != null) _context = context;
         }
 
         public void OnGet()
         {
+
+        }
+
+        public void OnPost(IFormFile uploadedFile)
+        {
+            if (uploadedFile.ContentType != "text/csv" || uploadedFile == null || uploadedFile.Length == 0)
+            {
+                Console.WriteLine("Wrong file format uploaded!");
+                // show some message for the user
+                return;
+            }
+
+            SourceDataManager sdm = new SourceDataManager(_context);
+            
+            if(sdm.LoadDbWithInputData(uploadedFile))
+            {
+                Console.WriteLine("Data loaded successfully!");
+                // display a message for the user
+            }
+            else
+            {
+                Console.WriteLine("Failed to load data from upload!");
+                // display a message for the user
+            }
 
         }
     }
