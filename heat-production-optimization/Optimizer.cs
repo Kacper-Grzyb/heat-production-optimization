18f37be3b0b753b6e6f1b4bd19c7d38f05ae9c8a
KacperGrzyb2137 <kgrzyb2860@gmail.com>
2024-04-28 15:23:52 +0200

    Changed boiler data structure
    I know it breaks SOLID but i think it might be better this way

--- heat-production-optimization/Optimizer.cs
+++ heat-production-optimization/Optimizer.cs
@@ -2,103 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using heat_production_optimization.Models;
 
-// namespace heat_production_optimization
-// {
-//     public class Optimizer
-//     {
-//         private GasBoiler gasBoiler;
-//         private OilBoiler oilBoiler;
-
-//         public Optimizer()
-//         {
-//             // Initialize GasBoiler and OilBoiler with their respective capacities and parameters
-//             gasBoiler = new GasBoiler("GB", maxHeat: 5, productionCost: 500, cO2Emission: 215, gasConsumption: 1.1);
-//             oilBoiler = new OilBoiler("OB", maxHeat: 4, productionCost: 700, cO2Emission: 265, oilConsumption: 1.2);
-//         }
-
-//         public OptimizeHeatProduction()
-//         {
-//             double remainingHeatDemand = heatDemand;
-
-//             // Check if GasBoiler alone can meet the demand
-//             if (gasBoiler.MaxHeat >= remainingHeatDemand)
-//             {
-//                 // GasBoiler can meet the demand
-//                 Console.WriteLine($"Using {gasBoiler.Name} to meet the heat demand.");
-//                 double gasProductionCost = gasBoiler.ProductionCost * remainingHeatDemand;
-//                 double gasCO2Emission = gasBoiler.CO2Emission * remainingHeatDemand;
-//                 return;
-//             }
-
-//             // Use GasBoiler as much as possible
-//             double gasHeatProduced = gasBoiler.MaxHeat;
-//             remainingHeatDemand -= gasHeatProduced;
-
-//             // Check if OilBoiler is needed to meet the remaining demand
-//             if (remainingHeatDemand > 0 && oilBoiler.MaxHeat >= remainingHeatDemand)
-//             {
-//                 // Both GasBoiler and OilBoiler are needed
-//                 Console.WriteLine($"Using {gasBoiler.Name} and {oilBoiler.Name} to meet the heat demand.");
-//                 double gasProductionCost = gasBoiler.ProductionCost * gasHeatProduced;
-//                 double gasCO2Emission = gasBoiler.CO2Emission * gasHeatProduced;
-//                 double oilProductionCost = oilBoiler.ProductionCost * remainingHeatDemand;
-//                 double oilCO2Emission = oilBoiler.CO2Emission * remainingHeatDemand;
-//                 double totalProductionCost = gasProductionCost + oilProductionCost;
-//                 double totalCO2Emission = gasCO2Emission + oilCO2Emission;
-//                 return;
-//             }
-
-//             // GasBoiler and OilBoiler combined cannot meet the demand
-//             Console.WriteLine("Insufficient heat production capacity.");
-//             
-//         }
-//     }
-// }
-
-using System;
-
 namespace heat_production_optimization
 {
     public class Optimizer
     {
-        private GasBoiler _gasBoiler;
-        private OilBoiler _oilBoiler;
-
-  //      public Optimizer(SourceDataDbContext context)
-  //      {
-		//	_gasBoiler = context.gasBoiler;
-		//	_oilBoiler = context.oilBoiler;
-		//}
-
+        private IUnit? _gasBoiler;
+        private IUnit? _oilBoiler;
+        
         public (double, double, double, double) OptimizeHeatProduction(double heatDemand, SourceDataDbContext context)
         {
-            double gasProductionCost = 0;
-            double gasCO2Emission = 0;
-            double oilProductionCost = 0;
-            double oilCO2Emission = 0;
-			_gasBoiler = context.gasBoiler;
-			_oilBoiler = context.oilBoiler;
-
-			if (heatDemand <= _gasBoiler.MaxHeat)
-            {
-                gasProductionCost = heatDemand * _gasBoiler.ProductionCost;
-                gasCO2Emission = heatDemand * _gasBoiler.CO2Emission;
-            }
-            else
-            {
-                gasProductionCost = _gasBoiler.MaxHeat * _gasBoiler.ProductionCost;
-                gasCO2Emission = _gasBoiler.MaxHeat * _gasBoiler.CO2Emission;
-
-                double remainingHeatDemand = heatDemand - _gasBoiler.MaxHeat;
-                double oilHeatProduced = Math.Min(remainingHeatDemand, _oilBoiler.MaxHeat);
-                oilProductionCost = oilHeatProduced * _oilBoiler.ProductionCost;
-                oilCO2Emission = oilHeatProduced * _oilBoiler.CO2Emission;
-
-                gasProductionCost += oilProductionCost;
-                gasCO2Emission += oilCO2Emission;
-            }
-
-            return (gasProductionCost, gasCO2Emission, oilProductionCost, oilCO2Emission);
+            return (1, 1, 1, 1);
         }
     }
 
--- heat-production-optimization/ProductionUnits.cs
+++ heat-production-optimization/ProductionUnits.cs
@@ -1,25 +1,35 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Xml.Linq;
 
 namespace heat_production_optimization
 {
     public interface IUnit
     {
+        public string? Alias { get; }
         public string? Name { get;}
-        public double MaxHeat { get;}
-        public int ProductionCost { get;}
-        public int CO2Emission { get;}
+        public double? MaxHeat { get;}
+        public double? MaxElectricity { get; }
+        public int? ProductionCost { get;}
+        public int? CO2Emission { get;}
+        public double? GasConsumption { get; }
+        public double? OilConsumption { get; }
     }
 
     public class GasBoiler : IUnit
     {
+        public string? Alias { get; }
         public string? Name { get;}
-        public double MaxHeat { get;}
-        public int ProductionCost { get;}
-        public int CO2Emission { get;}
-        public double GasConsumption { get;}
+        public double? MaxHeat { get;}
+        public int? ProductionCost { get;}
+        public int? CO2Emission { get;}
+        public double? GasConsumption { get;}
 
-        public GasBoiler(string name, double maxHeat, int productionCost, int cO2Emission, double gasConsumption)
+        public double? OilConsumption { get; } = null;
+        public double? MaxElectricity { get; } = null;
+
+        public GasBoiler(double maxHeat, int productionCost, int cO2Emission, double gasConsumption, string alias = "GB", string name = "Gas Boiler")
         {
+            Alias = alias;
             Name = name;
             MaxHeat = maxHeat;
             ProductionCost = productionCost;
@@ -30,14 +40,19 @@ namespace heat_production_optimization
 
     public class OilBoiler : IUnit
     {
+        public string? Alias { get; }
         public string? Name { get;}
-        public double MaxHeat { get;}
-        public int ProductionCost { get;}
-        public int CO2Emission { get;}
-        public double OilConsumption { get;}
+        public double? MaxHeat { get;}
+        public int? ProductionCost { get;}
+        public int? CO2Emission { get;}
+        public double? OilConsumption { get;}
 
-        public OilBoiler(string name, double maxHeat, int productionCost, int cO2Emission, double oilConsumption)
+        public double? GasConsumption { get; } = null;
+        public double? MaxElectricity { get; } = null;
+
+        public OilBoiler(double maxHeat, int productionCost, int cO2Emission, double oilConsumption, string alias = "OB", string name = "Oil Boiler")
         {
+            Alias = alias;
             Name = name;
             MaxHeat = maxHeat;
             ProductionCost = productionCost;
@@ -48,18 +63,22 @@ namespace heat_production_optimization
 
     public class GasMotor : IUnit
     {
+        public string? Alias { get; }
         public string? Name { get;}
-        public double MaxHeat { get;}
-        public double MaxEletricity { get;}
-        public int ProductionCost { get;}
-        public int CO2Emission { get;}
-        public double GasConsumption { get;}
+        public double? MaxHeat { get;}
+        public double? MaxElectricity { get;}
+        public int? ProductionCost { get;}
+        public int? CO2Emission { get;}
+        public double? GasConsumption { get;}
 
-        public GasMotor(string name, double maxHeat, double maxElectricity, int productionCost, int cO2Emission, double gasConsumption)
+        public double? OilConsumption { get; } = null;
+
+        public GasMotor(double maxHeat, double maxElectricity, int productionCost, int cO2Emission, double gasConsumption, string alias = "GM", string name = "Gas Motor")
         {
+            Alias = alias;
             Name = name;
             MaxHeat = maxHeat;
-            MaxEletricity = maxElectricity;
+            MaxElectricity = maxElectricity;
             ProductionCost = productionCost;
             CO2Emission = cO2Emission;
             GasConsumption = gasConsumption;
@@ -68,21 +87,49 @@ namespace heat_production_optimization
 
     public class ElectricBoiler : IUnit
     {
+        public string? Alias { get; }
         public string? Name { get;}
-        public double MaxHeat { get;}
-        public double MaxEletricity { get;}
-        public int ProductionCost { get;}
-        public int CO2Emission { get;}
+        public double? MaxHeat { get;}
+        public double? MaxElectricity { get;}
+        public int? ProductionCost { get;}
+        public int? CO2Emission { get;}
 
-        public ElectricBoiler(string name, double maxHeat, double maxElectricity, int productionCost, int cO2Emission)
+        public double? GasConsumption { get; } = null;
+        public double? OilConsumption { get; } = null;
+
+        public ElectricBoiler(double maxHeat, double maxElectricity, int productionCost, int cO2Emission, string alias = "EK", string name = "Electric Boiler")
         {
             Name = name;
             MaxHeat = maxHeat;
-            MaxEletricity = maxElectricity;
+            MaxElectricity = maxElectricity;
             ProductionCost = productionCost;
             CO2Emission = cO2Emission;
         }
     }
+
+    public class CustomBoiler : IUnit
+    {
+		public string? Alias { get; }
+		public string? Name { get; }
+		public double? MaxHeat { get; }
+		public double? MaxElectricity { get; }
+		public int? ProductionCost { get; }
+		public int? CO2Emission { get; }
+		public double? GasConsumption { get; }
+		public double? OilConsumption { get; }
+
+        public CustomBoiler(string? alias, string? name, double? maxHeat = null, double? maxElectricity = null, int? productionCost = null, int? cO2Emission = null, double? gasConsumption = null, double? oilConsumption = null)
+		{
+			Alias = alias;
+			Name = name;
+			MaxHeat = maxHeat;
+			MaxElectricity = maxElectricity;
+			ProductionCost = productionCost;
+			CO2Emission = cO2Emission;
+			GasConsumption = gasConsumption;
+			OilConsumption = oilConsumption;
+		}
+	}
 
 
 }
