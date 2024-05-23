47a8bf558663fc1175ce1a8f1568b5a17baf372d
Kacper-Grzyb <kgrzyb2860@gmail.com>
2024-05-23 21:01:25 +0200

    fixed boiler activations page assigning values to wrong boilers

--- heat-production-optimization/Pages/UnitActivations.cshtml.cs
+++ heat-production-optimization/Pages/UnitActivations.cshtml.cs
@@ -9,16 +9,19 @@ namespace heat_production_optimization.Pages
     {
         private readonly SourceDataDbContext _context;
         public List<UnitUsageDataModel> unitUsageData { get; set; }
+        public List<ProductionUnitDataModel> productionUnits { get; set; }
 
         public UnitActivationsModel(SourceDataDbContext context)
         {
             _context = context;
-            unitUsageData = context.unitUsage
+            unitUsageData = _context.unitUsage
                 .Include(i => i.DateInterval)
                 .Include(i => i.ActivationPercentages)
                     .ThenInclude(a => a.Unit)
                 .ToList();
             unitUsageData = unitUsageData.OrderBy(unit => unit.DateInterval.TimeFrom).ToList();
+
+            productionUnits = _context.productionUnits.ToList();
         }
 
         public void OnGet()
